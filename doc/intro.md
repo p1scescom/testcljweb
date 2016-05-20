a58507c648977493235377412801400ea2587fe0
pisces0317 <uu12321uu-pisces@yahoo.co.jp>
2016-05-20 22:56:18 +0900

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to testcljweb
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/testcljweb/core_test.clj
@@ -0,0 +1,7 @@
+(ns testcljweb.core-test
+  (:require [clojure.test :refer :all]
+            [testcljweb.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
